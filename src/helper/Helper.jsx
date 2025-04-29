1a98fa58564fc02e8c103d1d008f3e728d8256cd
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-04-29 15:32:01 +0530

    lsdjfdsf

--- src/helper/Helper.jsx
+++ src/helper/Helper.jsx
@@ -2,6 +2,6 @@
 
 // const baseurl = "https://jobpakki-backend.onrender.com";
 // const baseurl = "https://api.leadershipladder.in";
-// const baseurl = "http://api.jobkityaari.com";
-const baseurl = "http://82.25.109.68:5001";
+const baseurl = "http://api.jobkityaari.com";
+// const baseurl = "http://82.25.109.68:5001";
 export { baseurl };
